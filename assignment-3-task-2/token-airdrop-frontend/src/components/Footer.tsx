18a7d340c88bdcb5295107cd17f1d9e01ac4ea5f
Alex Kumancev <kumancev1@gmail.com>
2023-03-25 21:50:38 +0300

    fix footer styles

--- assignment-3-task-2/token-airdrop-frontend/src/components/Footer.tsx
+++ assignment-3-task-2/token-airdrop-frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className=" bg-grey-900 rounded-lg shadow">
+    <footer className=" bg-grey-900 rounded-lg mob:shadow">
       <div className="w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{' '}
