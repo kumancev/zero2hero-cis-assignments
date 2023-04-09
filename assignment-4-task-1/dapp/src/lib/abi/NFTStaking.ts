3f473fb0c84ab32de1054ca7270f060e5bcab7a1
Alex Kumancev <kumancev1@gmail.com>
2023-04-10 01:47:48 +0300

    init front for nft-staking contract

---
+++ assignment-4-task-1/dapp/src/lib/abi/NFTStaking.ts
@@ -0,0 +1,177 @@
+const STAKING_ABI = [
+  {
+    inputs: [
+      {
+        internalType: 'contract IERC20',
+        name: '_rewardsToken',
+        type: 'address',
+      },
+      {
+        internalType: 'contract IERC721',
+        name: '_nftCollection',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '_staker',
+        type: 'address',
+      },
+    ],
+    name: 'avaibleRewards',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'claimRewards',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '_user',
+        type: 'address',
+      },
+    ],
+    name: 'getStakedTokens',
+    outputs: [
+      {
+        components: [
+          {
+            internalType: 'address',
+            name: 'staker',
+            type: 'address',
+          },
+          {
+            internalType: 'uint256',
+            name: 'tokenId',
+            type: 'uint256',
+          },
+        ],
+        internalType: 'struct NFTStaking.StakedToken[]',
+        name: '',
+        type: 'tuple[]',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'nftCollection',
+    outputs: [
+      {
+        internalType: 'contract IERC721',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'rewardsToken',
+    outputs: [
+      {
+        internalType: 'contract IERC20',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '_tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'stake',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    name: 'stakerAddress',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    name: 'stakers',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: 'amountStaked',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'timeOfLastUpdate',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'unclaimedRewards',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '_tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'withdraw',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+] as const
+
+export default STAKING_ABI
---
+++ assignment-4-task-1/dapp/src/lib/config.ts
@@ -0,0 +1,57 @@
+import { getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { configureChains, createClient } from 'wagmi'
+import { bscTestnet } from 'wagmi/chains'
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
+
+import NFT_ABI from './abi/NFT'
+import STAKING_ABI from './abi/NFTStaking'
+
+const { chains, provider } = configureChains(
+  [bscTestnet],
+  [
+    jsonRpcProvider({
+      rpc: () => ({
+        http: process.env.NEXT_PUBLIC_RPC_URL ?? '',
+      }),
+    }),
+  ]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'NFT Staking',
+  chains,
+})
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+})
+
+interface Contract {
+  address: `0x${string}`
+  abi: any
+}
+
+const staking_contract: Contract = {
+  address:
+    (process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`) ??
+    '0x4e9Ed617d43Ed0a405Dc6defad55d4f77387b122',
+  abi: STAKING_ABI,
+}
+
+const nft_contract: Contract = {
+  address:
+    (process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`) ??
+    '0xd26DC61248914B9b7E9e1231b89757Eca7470D06',
+  abi: NFT_ABI,
+}
+
+export {
+  chains,
+  connectors,
+  staking_contract,
+  nft_contract,
+  provider,
+  wagmiClient,
+}
